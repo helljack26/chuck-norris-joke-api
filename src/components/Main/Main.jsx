2737525172f865e677450632ec0f7e26926befdb
helljack26 <helljack22@gmail.com>
2022-01-28 11:56:13 +0200

    remove scroll bar for main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,30 +6,32 @@ import { getJokeListFromApi } from '../../store/chuckApi/types';
 
 const Main = () => {
     const dispatch = useDispatch();
-    const jokeList = useSelector(state => state.chuckApi.jokeList)
-    return (
-        <main>
-            <div className={style.mainHeader}>
-                <img src="./img/icon/chucknorris_logo.png" alt="Logo" className={style.logo} />
-                <div>
-                    <h1>Hey!</h1>
-                    <p className={style.description}>Let's try to find a joke for you:</p>
-                </div>
+    const jokeList = useSelector(state => state.chuckApi.jokeList);
+    // const searchJoke = useSelector(state => state.chuckApi.searchJoke);
+    const isJokeList = Boolean(jokeList);
+    // const isSearchMin = Boolean(searchJoke.length < 3)
+
+    return <main>
+        <div className={style.mainHeader}>
+            <img src="./img/icon/chucknorris_logo.png" alt="Logo" className={style.logo} />
+            <div>
+                <h1>Hey!</h1>
+                <p className={style.description}>Let's try to find a joke for you:</p>
             </div>
-            <RadioGroup />
-            <button type='button' className={style.radioGroupBtn}
-                onClick={() => {
-                    dispatch(getJokeListFromApi())
-                }} >Get a joke</button>
-            <div className={style.jokesBlock}>
-                {jokeList !== [] ? jokeList.map((jokeData, key) => {
-                    return (
-                        <JokeBlockItem jokeData={jokeData} key={key} />
-                    )
-                }) : ''}
-            </div>
-        </main>
-    )
+        </div>
+        <RadioGroup />
+        <button type='button' className={style.radioGroupBtn}
+            onClick={() => dispatch(getJokeListFromApi())}
+        >Get a joke</button>
+        <div className={style.jokesBlock}>
+            {isJokeList ? jokeList.map((jokeData, key) => {
+                return (
+                    <JokeBlockItem jokeData={jokeData} key={key} />
+                )
+            }) : ''}
+        </div>
+    </main>
+
 }
 
 export default Main;
--- src/components/RadioGroup/RadioGroup.jsx
+++ src/components/RadioGroup/RadioGroup.jsx
@@ -16,56 +16,56 @@ const RadioGroup = () => {
     const [openCategories, setOpenCategories] = useState(false);
     const [openSearch, setOpenSearch] = useState(false);
 
-    return (
-        <form className={style.radioGroup}>
-            {/* Random */}
-            <div className={style.radioGroupItem}>
-                <input type="radio" className={style.radioBtn} id="random"
-                    name="contact" value="random" defaultChecked onClick={() => {
-                        return (setOpenCategories(false),
-                            setOpenSearch(false),
-                            dispatch(updateCategoryJoke('')),
-                            dispatch(updateSearchJoke('')))
-                    }} />
-                <label htmlFor="random">Random</label>
-            </div>
+    return <form className={style.radioGroup}>
+        {/* Random */}
+        <div className={style.radioGroupItem}>
+            <input type="radio" className={style.radioBtn} id="random"
+                name="contact" value="random" defaultChecked onClick={() => {
+                    return (setOpenCategories(false),
+                        setOpenSearch(false),
+                        dispatch(updateCategoryJoke('')),
+                        dispatch(updateSearchJoke('')))
+                }} />
+            <label htmlFor="random">Random</label>
+        </div>
 
-            {/* Categories */}
-            <div className={style.radioGroupItem}>
-                <input type="radio" className={style.radioBtn} id="fromCategories"
-                    name="contact" value="fromCategories" onClick={() => {
-                        return (setOpenCategories(true),
-                            setOpenSearch(false),
-                            dispatch(updateSearchJoke('')),
-                            dispatch(updateCategoryJoke(categoriesList[0])))
-                    }} />
-                <label htmlFor="fromCategories">From categories</label>
-            </div>
-            <div className={`${openCategories === false ? style.displayNone : style.displayBlock}`} >
-                <RadioGroupCategories />
-            </div>
+        {/* Categories */}
+        <div className={style.radioGroupItem}>
+            <input type="radio" className={style.radioBtn} id="fromCategories"
+                name="contact" value="fromCategories" onClick={() => {
+                    return (setOpenCategories(true),
+                        setOpenSearch(false),
+                        dispatch(updateSearchJoke('')),
+                        dispatch(updateCategoryJoke(categoriesList[0])))
+                }} />
+            <label htmlFor="fromCategories">From categories</label>
+        </div>
+        <div className={`${openCategories === false ? style.displayNone : style.displayBlock}`} >
+            <RadioGroupCategories />
+        </div>
 
-            {/* Search */}
-            <div className={style.radioGroupItem}>
-                <input type="radio" className={style.radioBtn} id="search"
-                    name="contact" value="search" onClick={() => {
-                        return (
-                            setOpenCategories(false),
-                            setOpenSearch(true),
-                            dispatch(updateCategoryJoke('')))
-                    }} />
-                <label htmlFor="search">Search</label>
-            </div>
-            <div className={`${openSearch === false ? style.displayNone : style.displayBlock}`}>
-                <input type="text"
-                    className={style.searchInput}
-                    placeholder='Free text search...'
-                    onKeyPress={(event) => handleKeyPress(event)}
-                    onInput={(event) => dispatch(updateSearchJoke(event.target.value))}
-                />
-            </div>
-        </form>
-    )
+        {/* Search */}
+        <div className={style.radioGroupItem}>
+            <input type="radio" className={style.radioBtn} id="search"
+                name="contact" value="search" onClick={() => {
+                    return (
+                        setOpenCategories(false),
+                        setOpenSearch(true),
+                        dispatch(updateCategoryJoke('')))
+                }} />
+            <label htmlFor="search">Search</label>
+        </div>
+        <div className={`${openSearch === false ? style.displayNone : style.displayBlock}`}>
+            <input type="text"
+                minLength={3}
+                required="required"
+                className={style.searchInput}
+                placeholder='Free text search...'
+                onKeyPress={(event) => handleKeyPress(event)}
+                onInput={(event) => dispatch(updateSearchJoke(event.target.value))}
+            />
+        </div>
+    </form>
 }
 
 export default RadioGroup;
