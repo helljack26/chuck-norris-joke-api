c567962ef92b3e692a7c157382844425759b47fa
helljack26 <helljack22@gmail.com>
2022-03-13 13:21:13 +0200

    Configure React app for deployment to GitHub Pages2

--- src/components/FavoriteHeaderTablet/FavoriteHeaderTablet.jsx
+++ src/components/FavoriteHeaderTablet/FavoriteHeaderTablet.jsx
@@ -1,27 +1,28 @@
 import './FavoriteHeaderTablet.css'
 import { setFavoriteListState } from '../../store/chuckApi/actions';
 import { useDispatch, useSelector } from 'react-redux';
-
+import burgerMenuOpen from '../img/icon/burgerMenu.svg'
+import burgerMenuClosed from '../img/icon/burgerMenuClose.svg'
 const FavoriteHeaderTablet = () => {
     const dispatch = useDispatch();
     const favoriteListStateBtn = useSelector(state => state.chuckApi.favoriteListStateBtn);
 
     const isOpenFavoriteList = favoriteListStateBtn === true;
     const isClosedFavoriteList = favoriteListStateBtn === false;
-    
-    return  <div className={`${'favoriteHeaderTablet'} ${isOpenFavoriteList ? 'favoriteHeaderTabletActive' : ''}`} >
-    <button className='favoriteBtn'
-        onClick={() => {
-            return isClosedFavoriteList ?
-                dispatch(setFavoriteListState(true)) :
-                dispatch(setFavoriteListState(false))
-        }}>
-        <img src={isClosedFavoriteList ?
-            './img/icon/burgerMenu.svg' :
-            './img/icon/burgerMenuClose.svg'} alt="Burger menu button" />
-    </button>
-    <p className='favoriteHeaderText'>Favorite</p>
-</div>
+
+    return <div className={`${'favoriteHeaderTablet'} ${isOpenFavoriteList ? 'favoriteHeaderTabletActive' : ''}`} >
+        <button className='favoriteBtn'
+            onClick={() => {
+                return isClosedFavoriteList ?
+                    dispatch(setFavoriteListState(true)) :
+                    dispatch(setFavoriteListState(false))
+            }}>
+            <img src={isClosedFavoriteList ?
+                { burgerMenuOpen } :
+                { burgerMenuClosed }} alt="Burger menu button" />
+        </button>
+        <p className='favoriteHeaderText'>Favorite</p>
+    </div>
 }
 
 export default FavoriteHeaderTablet;
